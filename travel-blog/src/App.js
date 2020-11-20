db09753fe04a3a2b00a919c20a2d36af8bf7c110
yusungsamuel <ssyyu@ucdavis.edu>
2020-11-19 20:05:41 -0800

    add navbar jsx and styling

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -5,6 +5,17 @@ import './App.scss';
 function App() {
   return (
     <div className="app">
+      <div className="nav-bar">
+        <div className="header">
+          <h1>Travelize</h1>
+          <p>My traveling experiences</p>
+        </div>
+        <div className="nav-list">
+          <a>Home</a>
+          <a>Blog</a>
+          <a className="button">About</a>
+        </div>
+      </div>
     </div>
   );
 }
