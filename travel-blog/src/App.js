59ed241317f9e6c9e656012ebab2f60ee5aca04e
yusungsamuel <ssyyu@ucdavis.edu>
2020-11-19 21:28:55 -0800

    add footer and media query to make site responsive

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -26,17 +26,23 @@ function App() {
             </p>
             <p>
               At erat pellentesque adipiscing commodo elit at imperdiet. Tortor posuere ac ut consequat semper. Nisi lacus sed viverra tellus in hac. Nunc aliquet bibendum enim facilisis gravida. Augue mauris augue neque gravida in. Nibh mauris cursus mattis molestie a. Ut etiam sit amet nisl purus in mollis nunc. Sodales ut eu sem integer vitae justo eget magna fermentum. Quis enim lobortis scelerisque fermentum. Purus semper eget duis at tellus at urna. Et tortor at risus viverra. Non odio euismod lacinia at quis risus. Amet justo donec enim diam vulputate ut.
-    </p>
+            </p>
             <p>
               Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Nam at lectus urna duis convallis. Sem nulla pharetra diam sit amet. Vestibulum lorem sed risus ultricies tristique nulla. Non blandit massa enim nec dui nunc mattis enim. Quis commodo odio aenean sed adipiscing diam donec. Ornare massa eget egestas purus viverra. Pellentesque eu tincidunt tortor aliquam nulla facilisi. Lectus sit amet est placerat in. Quis auctor elit sed vulputate mi sit.
-</p>
+            </p>
             <p>
               Iaculis urna id volutpat lacus laoreet non curabitur gravida arcu. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Quisque egestas diam in arcu cursus. Ultrices eros in cursus turpis massa tincidunt dui ut. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Praesent elementum facilisis leo vel fringilla est ullamcorper. Viverra justo nec ultrices dui sapien. Eu non diam phasellus vestibulum. Eu feugiat pretium nibh ipsum consequat. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Mollis nunc sed id semper risus in hendrerit gravida. Est ultricies integer quis auctor elit sed. Nunc consequat interdum varius sit amet mattis.
-</p>
+            </p>
           </div>
         </div>
       </div>
-
+      <div className="footer">
+        <p className="trademark">© 2020 Travelize</p>
+        <div className="links">
+          <a>Privacy Policy</a>
+          <a>Terms and conditions</a>
+        </div>
+      </div>
     </div>
   );
 }
